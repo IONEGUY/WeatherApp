afb084d66c3ae16b591b60c52ba91f8877c6893b
Ivan Zavadsky <i.zavadsky@itransition.com>
2020-09-08 13:33:38 +0300

    added strings and api constants

---
+++ WeatherApp/Resources/Strings.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+struct Strings {
+    static let alert = "Alert"
+    static let error = "Error"
+    static let ok = "Ok"
+    static let celsiusMarker = "°"
+    static let weatherDescriptionPattenr = "Today: %@. The temperature is %d°"
+    static let dayNameDateFormat = "EEEE"
+    static let today = "Today"
+    static let share = "Share"
+    static let hoursFormatWithMinutes = "HH:mm"
+    static let hoursFormat = "hh"
+    static let forecast = "Forecast"
+    static let unit = "metric"
+}
